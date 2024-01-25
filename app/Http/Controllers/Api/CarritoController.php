8a0187732a64850afb3d2bad12f4dccd183e1045
Jose Luis <jolutase104@gmail.com>
2024-01-25 19:29:15 +0100

    first commit

---
+++ app/Http/Controllers/Api/CarritoController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CarritoController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware(
+            'auth:api'
+        );
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json(['mensaje' => 'Accediendo a index']);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return response()->json(['mensaje' => 'Insertando'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return response()->json(['mensaje' => 'Ficha de ' . $id]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return response()->json(['mensaje' => 'Actualizando elemento']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Request $request,$id)
+    {
+        if($request->id_usuario){
+            
+        }else{
+
+        }
+        return response()->json(null,204);
+
+    }
+}
